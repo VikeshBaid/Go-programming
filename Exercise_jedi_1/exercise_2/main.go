94c42f0e916284287bdc0434190be404eb0f7b10
Vikesh Baid <vikesh.happy2@gmail.com>
2019-01-05 00:43:35 +0530

    update

---
+++ Exercise_jedi_1/exercise_2/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println("zero value of declared variables =", x)
+	fmt.Println("zero value of declared variables =", y)
+	fmt.Println("zero value of declared variables =", z)
+}
